eab45d7100867d866488858df88b50164f8def71
sampathGitHub1910 <saisampath1910@gmail.com>
2024-05-11 12:31:46 +0530

    apis

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express()
 const port = process.env.PORT || 5000
 
 app.use(express.json())
-app.use("/api/contacts", require("./routes/contactsRoutes"))
+app.use("/api/tasks", require("./routes/todoRoutes"))
 app.use("/api/users", require("./routes/userRoutes"))
 app.use(errorHandler)
 
